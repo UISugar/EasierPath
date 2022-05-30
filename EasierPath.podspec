234c2ce2428eeac9bf5bccd90703360c44b78c86
Fomagran <fomagran6@naver.com>
2022-05-30 16:19:24 -0500

    Update podspec

--- EasierPath.podspec
+++ EasierPath.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'EasierPath'
-  s.version          = '1.0.0'
+  s.version          = '1.0.1'
   s.summary          = 'To make it easier to use UIBezierPath ⚡️'
 
 
--- Example/EasierPath/ViewController.swift
+++ Example/EasierPath/ViewController.swift
@@ -10,32 +10,45 @@ import UIKit
 import EasierPath
 
 class ViewController: UIViewController {
-    
-    lazy var easierPath = EasierPath()
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .black
-        
+        setupUI()
         drawEasierPath()
         
-        view.layer.addSublayer(
-            easierPath.makeLayer(
-            lineWidth:3,
-            lineColor: .white,
-            fillColor:.systemPink)
-        )
+        let bezierPath:UIBezierPath = UIBezierPath()
+        bezierPath.move(to: CGPoint(x: 100, y: 100))
+        bezierPath.addLine(to: CGPoint(x: 200, y: 100))
+        bezierPath.addCurve(to: CGPoint(x: 200, y: 300), controlPoint1: CGPoint(x: 250, y: 150), controlPoint2: CGPoint(x: 175, y: 250))
+        bezierPath.addLine(to: CGPoint(x:100,y:300))
+        bezierPath.addCurve(to: CGPoint(x: 100, y: 100), controlPoint1: CGPoint(x: 125, y: 250), controlPoint2: CGPoint(x: 50, y: 150))
+        bezierPath.close()
+        
+        let caShapeLayer:CAShapeLayer = CAShapeLayer()
+        caShapeLayer.path = bezierPath.cgPath
+        caShapeLayer.strokeColor = UIColor.white.cgColor
+        caShapeLayer.fillColor = UIColor.systemPink.cgColor
+        caShapeLayer.lineWidth = 3
+        
+        view.layer.addSublayer(caShapeLayer)
+        
+    }
+    
+    func setupUI() {
+        view.backgroundColor = .black
     }
     
     func drawEasierPath() {
-        easierPath
-            .start(100, 100)
-            .curve(to: .right(200), .bezier(.rightUp(75, 50), .rightDown(150, 50)))
-            .down(100)
-            .curve(to:.left(200),.quad(.leftDown(100,50)))
-            .up(100)
-            .end()
+        let easierPath = EasierPath(100,100)
+            .right(100)
+            .curve(to: .down(200), .bezier(.rightDown(50,50), .leftDown(25,150)))
+            .left(100)
+            .curve(to:.up(200), .bezier(.rightUp(25,50), .leftUp(50,150)))
+        
+        let layer = easierPath.makeLayer(lineWidth:3,lineColor: .white,fillColor:.systemPink)
+        
+        view.layer.addSublayer(layer)
     }
 }
 
